b8fde7d22fe35be9c292bd90f8037f229b6689b7
Jonas Engström <56550304+JonasEngstrom@users.noreply.github.com>
2022-02-20 20:36:19 +0100

    A function to find the largest table.

---
+++ R/find_largest_table.R
@@ -0,0 +1,33 @@
+#' Find Largest Table
+#'
+#' Finds the largest table among the ones input from Medinet, as this is the
+#' most likely to contain the schedule data.
+#'
+#' @param tables HTML tables imported by *extract_tables*.
+#'
+#' @return An integer indicating which table has the largest area.
+#' @export
+#' @seealso [medinetparser::extract_tables()]
+#' @md
+#'
+#' @examples
+#' largest_table_index <- find_largest_table(tables)
+find_largest_table <- function(tables) {
+  table_sizes <- dplyr::tibble()
+
+  for (i in 1:length(tables)) {
+    table_sizes <- table_sizes %>%
+      dplyr::bind_rows(
+        tibble::as_tibble_row(
+          c(table_number = i,
+            table_area = dim(tables[[i]])[1] * dim(tables[[i]])[2]
+            )
+        )
+      )
+  }
+
+  table_sizes %>%
+    dplyr::filter(table_area == max(table_area)) %>%
+    pull(table_number) %>%
+    return()
+}
