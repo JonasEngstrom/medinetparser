bc214a8bfc276d451032d66f664f50e1d6cdd252
Jonas Engström <56550304+JonasEngstrom@users.noreply.github.com>
2022-02-22 21:33:57 +0100

    Small edit to help.

--- R/extract_tables.R
+++ R/extract_tables.R
@@ -3,10 +3,16 @@
 #' Loads the HTML file downloaded from Medinet and returns the HTML tables for
 #' further analysis. NB! This takes a while to run. Be patient.
 #'
+#' If you run into memory problems using this function, exporting fewer weeks
+#' at a time from Medinet is suggested. Tallies can easily be added if you are
+#' interested in analyzing a longer period of time. Tables showing distribution
+#' of shifts over time can be bound using dplyr’s `bind_cols()`.
+#'
 #' @param file_path The hath to the HTML file downloaded from Medinet.
 #'
 #' @return A list of HTML tables for use with rvest.
 #' @export
+#' @md
 #'
 #' @examples
 #' tables <- extract_tables('data.html')
