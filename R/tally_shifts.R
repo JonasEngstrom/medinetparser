efe4532ceac5904a2c9d626e1bfe2a01f6dd2df0
Jonas Engström <56550304+JonasEngstrom@users.noreply.github.com>
2022-02-22 22:27:21 +0100

    Adjustments to functions to make them work with schedules from several departments.

--- R/tally_shifts.R
+++ R/tally_shifts.R
@@ -19,7 +19,10 @@ tally_shifts <- function(table) {
         grepl(',', table[[1]]) & !grepl('\\d', table[[1]])
       )[1]:n()
     ) %>%
+    dplyr::mutate(X2 = if_else(grepl(',', X1), X1, '')) %>%
+    dplyr::na_if('') %>%
     tidyr::fill(X2) %>%
+    dplyr::filter(!(X1 %in% X2)) %>%
     tidyr::pivot_wider(names_from = X2,
                        values_from = X1,
                        names_repair = 'unique',
@@ -28,11 +31,12 @@ tally_shifts <- function(table) {
   return_table <- tibble::tibble()
 
   for (i in 1:length(temporary_table)) {
-    return_table <- temporary_table[[i]][[1]][1] %>%
+    return_table <- temporary_table[i] %>%
+      colnames() %>%
       tibble::as_tibble_col(column_name = 'Doctor') %>%
       dplyr::bind_cols(
         tibble::as_tibble_row(
-          temporary_table[[i]][[1]] %>% tail(-2) %>% table(), .name_repair = 'minimal'
+          temporary_table[[i]][[1]] %>% table(), .name_repair = 'minimal'
         )
       ) %>%
       dplyr::bind_rows(return_table)
