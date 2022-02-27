1c0689576cba9567a079b5456a81633bd910db19
Jonas Engström <56550304+JonasEngstrom@users.noreply.github.com>
2022-02-27 01:02:04 +0100

    First draft of date finding function.

---
+++ R/extract_dates.R
@@ -0,0 +1,38 @@
+#' Extract Dates
+#'
+#' Extracts the dates from a schedule extracted by `extract_table()`, setting the year of all the dates to the same year.
+#'
+#' @param schedule_table A schedule table extracted bu `extract_table()`
+#' @param first_year The first year of the dates, as found by `find_first_year()`.
+#'
+#' @return A tibble of the dates in the schedule.
+#' @export
+#' @seealso [medinetparser::extract_table()], [medinetparser::find_first_year()]
+#' @md
+#'
+#' @examples
+#' dates <- extract_dates(schedule_data, 2022)
+extract_dates <- function(schedule_table, first_year) {
+  return_table <- schedule_table %>%
+    dplyr::filter(
+      if_any(.fns = function(x) stringr::str_detect(x, '\\d{1,2}/\\d{1,2}'))
+      ) %>%
+    dplyr::mutate(
+      dplyr::across(
+        dplyr::everything(),
+        function(x) paste(
+          first_year,
+          stringr::str_extract(x, '\\d{1,2}$') %>%
+            stringr::str_pad(2, pad = '0'),
+          stringr::str_extract(x, '\\d{1,2}') %>%
+            stringr::str_pad(2, pad = '0'), sep = '-') %>%
+                    lubridate::ymd() %>%
+          return()
+        )
+      ) %>%
+    tidyr::pivot_longer(cols = dplyr::everything()) %>%
+    dplyr::distinct(value) %>%
+    tidyr::drop_na() %>%
+    dplyr::rename(date = value) %>%
+    return()
+}
