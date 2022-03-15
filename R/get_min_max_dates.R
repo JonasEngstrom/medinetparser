04e94685609ff0c20c2cee96195df1791cc9e1a8
Jonas Engström <56550304+JonasEngstrom@users.noreply.github.com>
2022-03-15 16:27:30 +0100

    Added get_min_max_dates function.

---
+++ R/get_min_max_dates.R
@@ -0,0 +1,22 @@
+#' Get Min Max Dates
+#'
+#' Returns a list of the first and last date in a schedule as returned by `load_tidy_schedule()`. Meant to be used for creating titles for plots.
+#'
+#' @param tidy_schedule A schedule as returned by `load_tidy_schedule()`
+#'
+#' @return A list where the first value is the first date in the schedule and the last value is the last value in the schedule.
+#' @export
+#' @seealso [medinetparser::load_tidy_schedule()]
+#' @md
+#'
+#' @examples
+#' min_max_dates <- get_min_max_dates(tidy_schedule)
+get_min_max_dates <- function(tidy_schedule) {
+  c(tidy_schedule %>%
+      dplyr::summarise(min(date)) %>%
+      dplyr::pull(),
+    tidy_schedule %>%
+      dplyr::summarise(max(date)) %>%
+      dplyr::pull()) %>%
+    return()
+}
