c31d8f43476fda377280ce25799d421bd33344ed
Jonas Engström <56550304+JonasEngstrom@users.noreply.github.com>
2022-03-15 16:01:09 +0100

    Added documentation.

--- R/plot_shifts_per_doctor.R
+++ R/plot_shifts_per_doctor.R
@@ -1,3 +1,30 @@
+#' Plot Shifts Per Doctor
+#'
+#' Plots a bar graph of the total shifts per doctor, stacked by shift type.
+#'
+#' @param tidy_schedule A tibble containing a schedule, as loade by
+#'   `load_tidy_schedule()`.
+#'
+#' @return A ggplot with a stacked barchart.
+#' @export
+#' @seealso [medinetparser::load_tidy_schedule()]
+#' @md
+#'
+#' @examples
+#' # Display graph right away.
+#' plot_shifts_per_doctor(tidy_schedule)
+#'
+#' # Plot only a subset of shifts.
+#' tidy_schedule %>%
+#'     filter(shift_type %in% c('Pjour', 'Bjour')) %>%
+#'     plot_shifts_per_doctor()
+#'
+#' # Same as above but also change axis label and save plot as
+#' # an object for later use.
+#' plot_of_shifts <- tidy_schedule %>%
+#'     filter(shift_type %in% c('Pjour', 'Bjour')) %>%
+#'     plot_shifts_per_doctor() +
+#'     ylab('Number of Night Shifts')
 plot_shifts_per_doctor <- function(tidy_schedule) {
   tidy_schedule %>%
     ggplot2::ggplot(aes(x = doctor_name %>%
