9f32b385410fbfe047407d6bd6a94e4f2394522c
Jonas Engström <j.e.engstrom@gmail.com>
2024-04-18 18:01:28 +0200

    Added skeleton for function plotting weekends.

---
+++ R/plot_weekend_work.R
@@ -0,0 +1,19 @@
+plot_weekend_work <- function(tidy_schedule,
+                              shift_types =
+                                c(
+                                  'PjourD',
+                                  'MjourD',
+                                  'Bjour',
+                                  'LångF',
+                                  'Pjour',
+                                  'Mjour'
+                                ),
+                              shift_days =
+                                c(
+                                  'Friday',
+                                  'Saturday',
+                                  'Sunday'
+                                )
+                              ) {
+
+}
